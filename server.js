f82cca8cf5d7ce2752371d399ea7ed015b41a228
soukainafl <soukaf173@gmail.com>
2020-04-06 14:19:39 +0100

    bien ajouter

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use('/',express.static(path.join(__dirname,'assets')));
 app.get('/salaire', (req, res) => {
-    
-
-        fs.readFile('./salaire.json', 'utf-8', function (err, data) {
-            if (err) throw err;
-    
-            var arrayOfObjects = JSON.parse(data);
-    
-           // res.send(arrayOfObjects);
-            console.log(arrayOfObjects);
-    
-        });
-    
     res.sendFile(path.join(__dirname, 'salaire.html'));
 
 });
@@ -30,20 +18,70 @@ app.get('/departement', (req, res) => {
 });
 
 
-// app.get('/userss', function (req, res) {
+ app.get('/userss', function (req, res) {
 
-//     fs.readFile('./department.json', 'utf-8', function (err, data) {
-//         if (err) throw err;
+     fs.readFile('./salaire.json', 'utf-8', function (err, data) {
+        if (err) throw err;
 
-//         var arrayOfObjects = JSON.parse(data);
+         var arrayOfObjects = JSON.parse(data);
 
-//         res.send(arrayOfObjects);
-//         console.log(arrayOfObjects);
+         res.send(arrayOfObjects);
+         console.log(arrayOfObjects);
 
-//     });
-// })
+    });
+ });
 
+ app.get('/departe', function (req, res) {
 
+    fs.readFile('./department.json', 'utf-8', function (err, data) {
+       if (err) throw err;
+
+        var arrayDepartement = JSON.parse(data);
+
+        res.send(arrayDepartement);
+        console.log("arrayDepartement" + arrayDepartement);
+
+   });
+});
+
+app.post('/salaire',function(req, res){
+    var id_dep = req.body.id_dep;
+    var Matricule = req.body.Matricule;
+    var nom = req.body.nom;
+    var Prenom = req.body.Prenom;
+    var Age = req.body.Age;
+    var salaire = req.body.salaire;
+
+    fs.writeFile('salarie.json',JSON.stringify(list1,null,5),(err)=>{
+        console.log(err);
+    });
+fs.readFile('salaire.json', 'utf-8', function (err, data) {
+	if (err) throw err;
+
+	var arrayOfObjects = JSON.parse(data);
+	arrayOfObjects.push({
+         id_dep : id_dep,
+         Matricule : Matricule,
+         nom: nom,
+         Prenom : Prenom,
+         Age : Age,
+         salaire : salaire
+    
+	});
+
+    console.log(arrayOfObjects);
+    
+    fs.writeFile('salaire.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+        if (err) throw err;
+        console.log('Done!');
+        res.sendFile(__dirname + "/salaire.html");
+  
+    });
+});
+    
+});
+
+app.use("/", express.static(__dirname + "/"));
 
 
 
